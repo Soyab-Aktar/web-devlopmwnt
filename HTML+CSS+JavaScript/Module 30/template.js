838cb6ae602ad20e805626c14bc27785e2171f34
Soyab-Aktar <iamsoyabcode23@gmail.com>
2024-12-27 18:21:20 +0530

    Module 30-3

---
+++ HTML+CSS+JavaScript/Module 30/template.js
@@ -0,0 +1,19 @@
+const firstName = "Soyab";
+const lastName = "Aktar";
+const name = firstName + " " + lastName;
+console.log(name);
+
+const a = 10;
+const b = 20;
+
+const result = "The Sum of " + a + " and " + b + " is " + (a + b);
+console.log(result);
+
+// other method
+const math = `The sum of ${a} and ${b} is ${a + b}`;
+console.log(math);
+
+const hello = `Welcome
+to our home
+its me Soyab`;
+console.log(hello);
