c0e5e445a0e42cfd4f7dbf7e787bbc3e2b501958
Soyab-Aktar <iamsoyabcode23@gmail.com>
2025-10-18 21:56:13 +0530

    currently on Module 55

---
+++ Backend/Module_55/simple_crud_client/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2>Simple CRUD Client</h2>
+      <form>
+        <input type="text" name="name" placeholder="Enter Name" />
+        <br />
+        <input type="email" name="email" placeholder="Enter Email" />
+        <br />
+        <input type="submit" value="" />
+      </form>
+    </>
+  );
+}
+
+export default App;
